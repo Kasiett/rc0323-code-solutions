0f0d2a02166d88c8861fe213596c233337bc3653
Kasiet Rakhimdinova <k.rakhimdinova@gmail.com>
2023-05-25 19:55:47 +0000

    Completed hot-button-component

---
+++ react-hot-button-component/react-hot-button-component-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HotBtn from './HotBtn';
+
+function App() {
+  return (
+    <div className="App">
+      <HotBtn />
+    </div>
+  );
+}
+
+export default App;
